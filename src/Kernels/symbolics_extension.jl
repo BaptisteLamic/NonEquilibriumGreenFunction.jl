b02bd3e08291f78b2de21f3a444f6a8daff855a7
Baptiste Lamic <baptiste.lamic@gmail.com>
2023-10-19 21:18:11 +0200

    Partially working. Bug in Metatheory ?

--- src/Kernels/symbolics_extension.jl
+++ src/Kernels/symbolics_extension.jl
@@ -5,7 +5,7 @@ using Symbolics: wrap
 
 using TermInterface
 using Metatheory
-using Symbolics: @rule
+using Metatheory: @rule
 
 import Symbolics: unwrap
 import SymbolicUtils: similarterm
@@ -13,13 +13,15 @@ import Base: zero, one, isequal, log, inv
 import LinearAlgebra: tr
 
 
-TermInterface.exprhead(::SymbolicUtils.BasicSymbolic{K}) where K = :call
-TermInterface.istree(x::SymbolicUtils.BasicSymbolic{K}) where K = SymbolicUtils.istree(x)
-TermInterface.operation(x::SymbolicUtils.BasicSymbolic{K}) where K = SymbolicUtils.operation(x)
-TermInterface.arguments(x::SymbolicUtils.BasicSymbolic{K}) where K = SymbolicUtils.arguments(x)
-
-function similarterm(x::Symbolic{K}, head, args; metadata = nothing)  where K <: AbstractOperator
-    similarterm(x, head, args, AbstractOperator ; metadata = metadata)
+TermInterface.exprhead(::SymbolicUtils.BasicSymbolic{K}) where {K} = :call
+TermInterface.istree(x::SymbolicUtils.BasicSymbolic{K}) where {K} = SymbolicUtils.istree(x)
+TermInterface.operation(x::SymbolicUtils.BasicSymbolic{K}) where {K} = SymbolicUtils.operation(x)
+TermInterface.arguments(x::SymbolicUtils.BasicSymbolic{K}) where {K} = SymbolicUtils.arguments(x)
+function similarterm(x::Symbolic{K}, head, args; metadata=nothing) where {K<:AbstractOperator}
+    similarterm(x, head, args, AbstractOperator; metadata=metadata)
+end
+function EGraphs.egraph_reconstruct_expression(::Type{BasicSymbolic{K}}, op, args; metadata=nothing, exprhead=nothing) where {K<:AbstractOperator}
+    SymbolicUtils.Term{K}(op, args)
 end
 
 @symbolic_wrap struct SymbolicOperator <: AbstractOperator
@@ -34,62 +36,62 @@ unwrap(x::SymbolicOperator) = x.val
     similarterm(x, +, [x],)
 end
 @wrapped function +(x::AbstractOperator, y::AbstractOperator)
-    if x  isa Number && y isa Number
-        x+y
+    if x isa Number && y isa Number
+        x + y
     else
         if x isa Number
-            similarterm(y, +, [x,y],)
-        else 
-            similarterm(x, +, [x,y],)
+            similarterm(y, +, [x, y],)
+        else
+            similarterm(x, +, [x, y],)
         end
     end
 end
 @wrapped function +(x::Number, y::AbstractOperator)
-    similarterm(y, +, [x,y],)
+    similarterm(y, +, [x, y],)
 end
 @wrapped function +(x::AbstractOperator, y::Number)
-    similarterm(x, +, [x,y],)
+    similarterm(x, +, [x, y],)
 end
 
 @wrapped function -(x::AbstractOperator)
     similarterm(x, -, [x],)
 end
 @wrapped function -(x::AbstractOperator, y::AbstractOperator)
-    if x  isa Number && y isa Number
-        x-y
+    if x isa Number && y isa Number
+        x - y
     else
         if x isa Number
-            similarterm(y, -, [x,y],)
-        else 
-            similarterm(x, -, [x,y],)
+            similarterm(y, -, [x, y],)
+        else
+            similarterm(x, -, [x, y],)
         end
     end
 end
 @wrapped function -(x::Number, y::AbstractOperator)
-   similarterm(y, -, [x,y],)
+    similarterm(y, -, [x, y],)
 end
 @wrapped function -(x::AbstractOperator, y::Number)
-   similarterm(x, -, [x,y],)
+    similarterm(x, -, [x, y],)
 end
 
 
 
 @wrapped function *(x::AbstractOperator, y::AbstractOperator)
-    if x  isa Number && y isa Number
-        x*y
+    if x isa Number && y isa Number
+        x * y
     else
         if x isa Number
-            similarterm(y, *, [x,y],)
-        else 
-            similarterm(x, *, [x,y],)
+            similarterm(y, *, [x, y],)
+        else
+            similarterm(x, *, [x, y],)
         end
     end
 end
 @wrapped function *(x::Number, y::AbstractOperator)
-   similarterm(y, *, [x,y],)
+    similarterm(y, *, [x, y],)
 end
 @wrapped function *(x::AbstractOperator, y::Number)
-   similarterm(x, *, [x,y],)
+    similarterm(x, *, [x, y],)
 end
 
 @wrapped function inv(G::AbstractOperator)
@@ -105,7 +107,7 @@ end
     similarterm(G, tr, [G])
 end
 @wrapped function (/)(left::AbstractOperator, right::AbstractOperator)
-    return   similarterm(left, /, [left, right], )
+    return similarterm(left, /, [left, right],)
 end
 
 SymbolicUtils.simplify(n::SymbolicOperator; kw...) = wrap(SymbolicUtils.simplify(unwrap(n); kw...))
@@ -115,7 +117,7 @@ substitute(expr::SymbolicOperator, s::Pair; kw...) = wrap(substituter(s)(unwrap(
 substitute(expr::SymbolicOperator, s::Vector; kw...) = wrap(substituter(s)(unwrap(expr); kw...))
 substitute(expr::SymbolicOperator, s::Dict; kw...) = wrap(substituter(s)(unwrap(expr); kw...))
 SymbolicUtils.Code.toexpr(x::SymbolicOperator) = SymbolicUtils.Code.toexpr(unwrap(x))
-SymbolicUtils.Code.toexpr(x::SymbolicOperator,st) = SymbolicUtils.Code.toexpr(unwrap(x),st)
+SymbolicUtils.Code.toexpr(x::SymbolicOperator, st) = SymbolicUtils.Code.toexpr(unwrap(x), st)
 SymbolicUtils.setmetadata(x::SymbolicOperator, t, v) = wrap(SymbolicUtils.setmetadata(unwrap(x), t, v))
 SymbolicUtils.getmetadata(x::SymbolicOperator, t) = SymbolicUtils.getmetadata(unwrap(x), t)
 SymbolicUtils.hasmetadata(x::SymbolicOperator, t) = SymbolicUtils.hasmetadata(unwrap(x), t)
@@ -128,51 +130,49 @@ function Base.zero(::typeof(AbstractOperator))
 end
 
 simplify_kernel(expr) = expr |> unwrap |> _simplify_kernel |> wrap
-function _simplify_kernel(expr)
-    is_number(x) = x isa Number
-    is_operator(::BasicSymbolic{K}) where K <: AbstractOperator = true  
-    is_operator(::K) where K <: AbstractOperator = true  
-    is_operator(x) = false  
-    rules = [
-        @acrule 0 * ~x => 0
-        @acrule 0 + ~x => ~x
-        @acrule ~x + 0 => ~x
-        @rule 0 - ~x => -(~x)
-        @rule ~x - 0 => ~x
-        @rule ~x - ~x => 0
-        @rule 1 * ~x => ~x
-        @rule -1 * ~x => - ~x
-        @rule -( ~a + ~b) => - (~a) - (~b) 
-        @rule ~c - ( ~a + ~b) => ~c - (~a) - (~b) 
-        @rule -( ~a - ~b) => - (~a) + (~b) 
+function _simplify_kernel(expr::BasicSymbolic{K}) where {K}
+    theory = @theory x y z a b c m n begin
+        0 * x => 0
+        0 + x => x
+        x + 0 => x
+        0 - x => -(x)
+        x - 0 => x
+        x - x => 0
+        1 * x => x
+        -1 * x => -x
+        -(a + b) => -(a) - (b)
+        c - (a + b) => c - (a) - (b)
+        -(a - b) => -(a) + (b)
 
-        @rule ~x * ~n::is_number => ~n * ~x
-        @acrule ~n::is_number * ~x + ~m::is_number * ~x  => (~n+m) * ~x
-        @rule ~x + ~x  => 2 * ~x
-        @rule ~n::is_number * ~x + ~x  => (~n+1) * ~x
-        @rule ~n::is_number * ~x - ~x  => (~n-1) * ~x
-        @rule ~a::is_number * ~b::is_number * ~z => (~a * ~b) * ~z
-        @rule ~n::is_number * (~x + ~y)  =>  ~n * ~x + ~n* ~y
-
-        @rule inv(inv(~a::is_operator)) => ~a 
-    ] #|> SymbolicUtils.Chain |> SymbolicUtils.Postwalk |>  SymbolicUtils.Fixpoint
-    Metatheory.rewrite(expr, rules)
+        x * n::Number => n * x
+        n::Number * x + m::Number * x => (n + m) * x
+        x + x => 2 * x
+        n::Number * x + x => (n + 1) * x
+        n::Number * x - x => (n - 1) * x
+        a::Number * b::Number * z => (a * b) * z
+        n::Number * (x + y) => n * x + n * y
+        inv(inv(a::BasicSymbolic{Kernel})) => a 
+    end
+    g = EGraph(expr)
+    settermtype!(g, BasicSymbolic{K})
+    report = saturate!(g, theory)
+    extract!(g, astsize)
 end
 
-Base.isequal(a::SymbolicOperator,b::SymbolicOperator) = isequal(unwrap(a), unwrap(b))
+Base.isequal(a::SymbolicOperator, b::SymbolicOperator) = isequal(unwrap(a), unwrap(b))
 convert(::Type{SymbolicOperator}, x::Number) = SymbolicOperator(x)
-function Base.promote_rule(::Type{SymbolicOperator}, ::Type{K}) where K<:Number 
-     return SymbolicOperator
+function Base.promote_rule(::Type{SymbolicOperator}, ::Type{K}) where {K<:Number}
+    return SymbolicOperator
 end
 #Dirty ?
-function *(A::Matrix{SymbolicOperator},B::Matrix{SymbolicOperator})
-    return wrap.(unwrap.(A)*unwrap.(B))
+function *(A::Matrix{SymbolicOperator}, B::Matrix{SymbolicOperator})
+    return wrap.(unwrap.(A) * unwrap.(B))
 end
-function *(A::Matrix,B::Matrix{SymbolicOperator})
-    return wrap.(A*unwrap.(B))
+function *(A::Matrix, B::Matrix{SymbolicOperator})
+    return wrap.(A * unwrap.(B))
 end
-function *(A::Matrix{SymbolicOperator},B::Matrix)
-    return wrap.(unwrap.(A)*B)
+function *(A::Matrix{SymbolicOperator}, B::Matrix)
+    return wrap.(unwrap.(A) * B)
 end
 
 
@@ -188,7 +188,7 @@ Base.display(A::SymbolicOperator) = display(unwrap(A))
     @test zero(G) == 0
     A = [G G; G G]
     @test A isa Matrix{SymbolicOperator}
-    @test isequal(tr(A) |> simplify_kernel, 2G )
+    @test isequal(tr(A) |> simplify_kernel, 2G)
     @test (A * A) isa Matrix{SymbolicOperator}
 end
 
@@ -198,8 +198,8 @@ end
     using LinearAlgebra
     @variables G::Kernel Σ::Kernel
     @test isequal(inv(inv(G)) |> simplify_kernel, G)
-    @test isequal(simplify_kernel( G - G), 0)
-    @test isequal(simplify_kernel( G + G + G - G),2G)
+    @test isequal(simplify_kernel(G - G), 0)
+    @test isequal(simplify_kernel(G + G + G - G), 2G)
 end
 
 @testitem "Construction of the current observable" begin
@@ -210,7 +210,7 @@ end
     τz = [1 2; 2 3] // 2
     G = [0 G_R'; G_R G_K]
     Σl = [Σ_K Σ_R; Σ_R' 0]
-    expr = simplify_kernel(-tr(τz * (G * Σl - Σl * G)))
+    expr = simplify_kernel(tr(τz * (G * Σl - Σl * G)))
     @test expr isa SymbolicOperator
     bs, N, Dt = 2, 128, 2.0
     ax = LinRange(-Dt / 2, Dt, N)
@@ -218,6 +218,6 @@ end
     GR = RetardedKernel(ax, A, bs, NONCompression())
     GK = AcausalKernel(ax, A, bs, NONCompression())
     f = build_function(expr, G_R, G_K, Σ_R, Σ_K, expression=Val{false})
-    f(GR,GK,GR,GK) isa Vector{ComplexF32}
+    f(GR, GK, GR, GK) isa Vector{ComplexF32}
 end
 
--- test/debug.jl
+++ test/debug.jl
@@ -1,10 +1,12 @@
-
-using LinearAlgebra
 using Revise
 using NonEquilibriumGreenFunction
 using Symbolics
-@variables η
-D = Differential(η)
-@variables a::Kernel
-expr = :(0*a + a)
-simplify_kernel(expr)
+using LinearAlgebra
+@variables G_R::Kernel G_K::Kernel
+@variables Σ_R::Kernel Σ_K::Kernel
+τz = [1 2; 2 3] // 2
+G = [0 G_R'; G_R G_K]
+Σl = [Σ_K Σ_R; Σ_R' 0]
+expr = simplify_kernel(-tr(τz * (G * Σl)))
+expr isa SymbolicOperator
+f = build_function(expr, G_R, G_K, Σ_R, Σ_K, expression=Val{false})
