eac61ae48e9cdbb9cd2651f82cfd2735c38d4c36
Baptiste Lamic <baptiste.lamic@gmail.com>
2025-03-07 19:27:08 +0100

    rework extract_blockdiag

--- src/utils.jl
+++ src/utils.jl
@@ -1,62 +1,64 @@
 
-@inline blockrange(i,bs) = (i-1)*bs+1:i*bs
-@inline function blockindex(p::Number,bs)
-    a,i = divrem(p-1,bs)
-    return (a+1,i+1)
+@inline blockrange(i, bs) = (i-1)*bs+1:i*bs
+@inline function blockindex(p::Number, bs)
+    a, i = divrem(p - 1, bs)
+    return (a + 1, i + 1)
 end
-function blockindex(tp,bs)
-    r_block = zeros(eltype(tp),length(tp))
-    r_inblock = zeros(eltype(tp),length(tp))
+function blockindex(tp, bs)
+    r_block = zeros(eltype(tp), length(tp))
+    r_inblock = zeros(eltype(tp), length(tp))
     for i = 1:length(tp)
-        (r_block[i],r_inblock[i]) = blockindex(tp[i],bs)
+        (r_block[i], r_inblock[i]) = blockindex(tp[i], bs)
     end
-    return (r_block,r_inblock)
+    return (r_block, r_inblock)
 end
-function _extract_blockdiag(m::AbstractMatrix{T},bs,d::Int = 0) where T
-    @assert size(m,1) == size(m,2)
-    N = div(size(m,1), bs)
-    if abs(d) >= N
-        return spzeros(T,size(m)...)
-    else
-        shift_I = d < 0 ? abs(d) : 0
-        shift_J = d > 0 ? d : 0
-        J = [(blk+shift_J-1)*bs+j for j = 1:bs,p = 1:bs, blk = 1:N-abs(d) ]
-        J = reshape(J,:)
-        I = [(blk+shift_I-1)*bs+j for p = 1:bs, j = 1:bs, blk = 1:N-abs(d) ]
-        I = reshape(I,:)
-        V = [m[I[k],J[k]] for k = 1:length(I)]
-        return sparse(I,J,V,size(m)...)
+function sparse_extract_blockdiag(m::AbstractMatrix{T}, bs, diagonalIndices=0) where {T}
+    @assert size(m, 1) == size(m, 2)
+    N = div(size(m, 1), bs)
+    I = Vector{Int}()
+    J = Vector{Int}()
+    for d in diagonalIndices
+        if abs(d) <= N
+            shift_I = d < 0 ? abs(d) : 0
+            shift_J = d > 0 ? d : 0
+            newJ = [(blk + shift_J - 1) * bs + j for j = 1:bs, p = 1:bs, blk = 1:N-abs(d)]
+            J = [J; reshape(newJ, :)]
+            newI = [(blk + shift_I - 1) * bs + j for p = 1:bs, j = 1:bs, blk = 1:N-abs(d)]
+            I = [I; reshape(newI, :)]
+        end
     end
+    V = [m[I[k], J[k]] for k = 1:length(I)]
+    return sparse(I, J, V, size(m)...)
 end
-function extract_blockdiag(m::AbstractMatrix{T},bs,d = 0; compression = HssCompression()) where T
-    return sum(_extract_blockdiag(m,bs,_d) for _d in d) |> compression
+function extract_blockdiag(m::AbstractMatrix{T}, bs, d=0; compression=HssCompression()) where {T}
+    return compression(sparse_extract_blockdiag(m, bs, d))
 end
-function blockdiag(A::AbstractArray{T,3}, d::Integer = 0;compression = HssCompression()) where T
+function blockdiag(A::AbstractArray{T,3}, d::Integer=0; compression=HssCompression()) where {T}
     shift_I = d < 0 ? abs(d) : 0
     shift_J = d > 0 ? d : 0
-    @assert size(A,1) == size(A,2)
-    bs = size(A,1)
-    N = size(A,3)
-    I = [(t+shift_I-1)*bs+ib for ib in 1:bs, jb in 1:bs, t = 1:N]
-    J = [(t+shift_J-1)*bs+jb for ib in 1:bs, jb in 1:bs, t = 1:N]
-    return sparse(I[:],J[:],A[:],(N+abs(d))*bs,(N+abs(d))*bs) |> compression
+    @assert size(A, 1) == size(A, 2)
+    bs = size(A, 1)
+    N = size(A, 3)
+    I = [(t + shift_I - 1) * bs + ib for ib in 1:bs, jb in 1:bs, t = 1:N]
+    J = [(t + shift_J - 1) * bs + jb for ib in 1:bs, jb in 1:bs, t = 1:N]
+    return sparse(I[:], J[:], A[:], (N + abs(d)) * bs, (N + abs(d)) * bs) |> compression
 end
-function blockdiag(A::AbstractArray{<:AbstractMatrix{T},1}, d::Integer = 0;compression = HssCompression()) where T
-    _A = zeros(T,size(A[1])..., length(A))
+function blockdiag(A::AbstractArray{<:AbstractMatrix{T},1}, d::Integer=0; compression=HssCompression()) where {T}
+    _A = zeros(T, size(A[1])..., length(A))
     for p = 1:length(A)
-        for j = 1:size(A[1],2)
-            for i = 1:size(A[1],1)
-                _A[i,j,p] = A[p][i,j]
+        for j = 1:size(A[1], 2)
+            for i = 1:size(A[1], 1)
+                _A[i, j, p] = A[p][i, j]
             end
         end
     end
-    blockdiag(_A,d,compression = compression)
+    blockdiag(_A, d, compression=compression)
 end
 
-function _adapt(::HssMatrix,a)
+function _adapt(::HssMatrix, a)
     return hss(a)
 end
-function _adapt(::G,a) where {G<:AbstractArray}
+function _adapt(::G, a) where {G<:AbstractArray}
     return G(a)
 end
 function _adapt(a::AbstractArray)
