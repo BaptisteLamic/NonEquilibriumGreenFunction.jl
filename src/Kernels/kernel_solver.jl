985dc386dd87b8c0e78a31fecb2a2e5536df5eed
Baptiste Lamic <baptiste.lamic@gmail.com>
2025-04-16 18:58:48 +0200

    Reduce memory usage in solve_dyson

--- src/Kernels/kernel_solver.jl
+++ src/Kernels/kernel_solver.jl
@@ -1,4 +1,6 @@
 
+
+
 """
     Solve the equation  G = g + K⋅G  for G
 """
@@ -11,7 +13,7 @@ function solve_dyson(g::Kernel,K::Kernel)
     eye = cp(sparse(scalartype(K)(1)*I,size(diag_K)...)) #bypass limitation of HssMatrices.jl
     left = cp( eye - scalartype(K)(step(K)) * (matrix(K) - 1//2 * diag_K) )
     right = cp( matrix(g) -1 // 2 * diag_g)
-    sol_biased = left\right
-    correction = cp( diag_g - extract_blockdiag( sol_biased,bs, compression = cp) )
+    sol_biased = ldiv!(left,right)
+    correction = diag_g - extract_blockdiag( sol_biased,bs, compression = cp)
     return similar(g, cp(sol_biased + correction) )
 end
