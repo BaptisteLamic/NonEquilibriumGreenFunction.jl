ea44dae74fa6e51136fa9521357c7bc238d93dfc
Baptiste Lamic <baptiste.lamic@gmail.com>
2025-11-17 07:29:06 +0100

    Revert "Remove Symbolics Extension and Related Tests"
    This reverts commit cb45cd3158515fc2665ec98495a845bb810a26d5.

--- src/Kernels/symbolics_extension.jl
+++ src/Kernels/symbolics_extension.jl
@@ -1,131 +1,236 @@
 export SymbolicOperator
 
-using SymbolicUtils: Symbolic, BasicSymbolic
-using Symbolics: wrap
+using SymbolicUtils
+import TermInterface: maketerm, head, children, operation, arguments, isexpr, iscall
 
-import Symbolics: unwrap
+export maketerm, make_leaf
 
-import SymbolicUtils: similarterm
 import Base: zero, one, isequal, log, inv
 import LinearAlgebra: tr
+import Base: +, -, *, /, adjoint
 
+using Moshi.Match: @match
+using Moshi.Data: @data
+using Moshi.Derive: @derive
+@data SymbolicExpression begin
+    struct Node
+        head
+        operation #TODO: make that type stable
+        args::Vector{SymbolicExpression.Type}
+    end
+    Leaf(Any)
+end
+@derive SymbolicExpression[Hash, Eq, Show]
 
-function similarterm(x::Symbolic{K}, head, args; metadata = nothing)  where K <: AbstractOperator
-    similarterm(x, head, args, AbstractOperator ; metadata = metadata)
+function head(x::SymbolicExpression.Type)
+    @match x begin
+        SymbolicExpression.Node(head, _, _) => head
+        SymbolicExpression.Leaf(_) => error("Leaf has no head")
+    end
+end
+function children(x::SymbolicExpression.Type)
+    @match x begin
+        SymbolicExpression.Node(_, operation, args) => return [operation; args]
+        SymbolicExpression.Leaf(_) => error("Leaf has no children")
+    end
+end
+function operation(x::SymbolicExpression.Type)
+    @match x begin
+        SymbolicExpression.Node(_, operation, _) => operation
+        SymbolicExpression.Leaf(_) => error("Leaf has no operation")
+    end
+end
+function arguments(x::SymbolicExpression.Type)
+    @match x begin
+        SymbolicExpression.Node(_, _, arguments) => arguments
+        SymbolicExpression.Leaf(_) => error("Leaf has no arguments")
+    end
+end
+function isexpr(x::SymbolicExpression.Type)
+    @match x begin
+        SymbolicExpression.Node(_, _, _) => true
+        SymbolicExpression.Leaf(_) => false
+    end
+end
+function iscall(x::SymbolicExpression.Type)
+    @match x begin
+        SymbolicExpression.Node(head, _, _) => head == :call
+        SymbolicExpression.Leaf(_) => false
+    end
+end
+function maketerm(::Type{SymbolicExpression.Type}, head, args, metadata = nothing)
+    operation = args[1]
+    arguments::Vector{SymbolicExpression.Type} = args[2:end]
+    return SymbolicExpression.Node(head, operation, arguments)
 end
 
-@symbolic_wrap struct SymbolicOperator <: AbstractOperator
-    val
+@testitem "SymbolicExpression TermInterface" begin
+    using Symbolics
+    using TermInterface
+    using NonEquilibriumGreenFunction.SymbolicExpression
+    leaf_a = SymbolicExpression.Leaf(:a)
+    leaf_b = SymbolicExpression.Leaf(:b)
+    kernel = SymbolicExpression.Node(:call, *, [leaf_a, leaf_b])
+    @test kernel == SymbolicExpression.Node(:call, *, [leaf_a, leaf_b])
+    @test head(kernel) == :call
+    @test children(kernel) == [*, leaf_a, leaf_b]
+    @test operation(kernel) == *
+    @test arguments(kernel) == [leaf_a, leaf_b]
+    @test isexpr(kernel)
+    @test !isexpr(leaf_a)
+    @test iscall(kernel)
+    kernel_bis = maketerm(typeof(kernel), head(kernel), children(kernel), nothing)
+    @test head(kernel_bis) == head(kernel)
+    @show children(kernel_bis)
+    @test children(kernel_bis) == children(kernel)
+    @test operation(kernel_bis) == operation(kernel)
+    @test arguments(kernel_bis) == arguments(kernel)
+    @test kernel == kernel_bis
 end
-Base.:(==)(a::SymbolicOperator, b::SymbolicOperator) = unwrap(a) == unwrap(b)
-Base.:hash(a::SymbolicOperator) = hash(a.val)
-unwrap(x::SymbolicOperator) = x.val
-#=wrapper_type(::Type{AbstractOperator}) = SymbolicOperator
-symtype(::SymbolicOperator) = Kernel=#
+
+struct SymbolicOperator
+    expr::SymbolicExpression.Type
+end
+
+function SymbolicOperator(operation, args::Vector{SymbolicOperator})
+    expression = SymbolicExpression.Node(:call, operation, unwrap.(args))
+    return SymbolicOperator(expression)
+end
+
+function (==)(a::SymbolicOperator, b::SymbolicOperator)
+    return a.expr == b.expr
+end
+
+function make_leaf(value)
+    return SymbolicOperator(SymbolicExpression.Leaf(value))
+end
+
+function wrap(x::SymbolicExpression.Type)
+    return SymbolicOperator(x)
+end
+function unwrap(x::SymbolicOperator)
+    return x.expr
+end
+function unwrap(::Type{SymbolicOperator})
+    return SymbolicExpression.Type
+end
+wrap(x) = x
+unwrap(x) = x
+
+head(x::SymbolicOperator) = x |> unwrap |> head  |> wrap
+children(x::SymbolicOperator) = x |> unwrap |> children .|> wrap
+operation(x::SymbolicOperator) = x |> unwrap |> operation |> wrap
+arguments(x::SymbolicOperator) = x |> unwrap |> arguments .|> wrap
+isexpr(x::SymbolicOperator) = x |> unwrap |> isexpr
+iscall(x::SymbolicOperator) = x |> unwrap |> iscall
+function maketerm(::Type{T}, head, args, metadata = nothing) where T <: SymbolicOperator
+      wrap(maketerm(unwrap(T), head, unwrap.(args), metadata))
+end
+
+@testitem "SymbolicOperator TermInterface" begin
+    using Symbolics
+    using TermInterface
+    leaf_a = make_leaf(:a)
+    @test leaf_a == make_leaf(:a)
+    leaf_b = make_leaf(:b)
+    kernel = SymbolicOperator(*, [leaf_a, leaf_b])
+    @test head(kernel) == :call
+    @test children(kernel) == [*, leaf_a, leaf_b]
+    @test operation(kernel) == *
+    @test arguments(kernel) == [leaf_a, leaf_b]
+    @test isexpr(kernel)
+    @test iscall(kernel)
+    @test kernel == maketerm(typeof(kernel), head(kernel), children(kernel), nothing)
+end
 
 
-@wrapped function +(x::AbstractOperator)
-    similarterm(x, +, [x],)
+function +(x::SymbolicOperator)
+    SymbolicOperator(+, [x])
 end
-@wrapped function +(x::AbstractOperator, y::AbstractOperator)
+ function +(x::SymbolicOperator, y::SymbolicOperator)
     if x  isa Number && y isa Number
         x+y
     else
         if x isa Number
-            similarterm(y, +, [x,y],)
+            SymbolicOperator(+, [x,y])
         else 
-            similarterm(x, +, [x,y],)
+            SymbolicOperator(+, [x,y])
         end
     end
 end
-@wrapped function +(x::Number, y::AbstractOperator)
-    similarterm(y, +, [x,y],)
+ function +(x::Number, y::SymbolicOperator)
+    SymbolicOperator(+, [x,y])
 end
-@wrapped function +(x::AbstractOperator, y::Number)
-    similarterm(x, +, [x,y],)
+ function +(x::SymbolicOperator, y::Number)
+    SymbolicOperator(+, [x,y])
 end
 
-@wrapped function -(x::AbstractOperator)
-    similarterm(x, -, [x],)
+ function -(x::SymbolicOperator)
+    SymbolicOperator(-, [x])
 end
-@wrapped function -(x::AbstractOperator, y::AbstractOperator)
+ function -(x::SymbolicOperator, y::SymbolicOperator)
     if x  isa Number && y isa Number
         x-y
     else
         if x isa Number
-            similarterm(y, -, [x,y],)
+            SymbolicOperator(-, [x,y])
         else 
-            similarterm(x, -, [x,y],)
+            SymbolicOperator(-, [x,y])
         end
     end
 end
-@wrapped function -(x::Number, y::AbstractOperator)
-   similarterm(y, -, [x,y],)
+ function -(x::Number, y::SymbolicOperator)
+   SymbolicOperator(-, [x,y])
 end
-@wrapped function -(x::AbstractOperator, y::Number)
-   similarterm(x, -, [x,y],)
+ function -(x::SymbolicOperator, y::Number)
+   SymbolicOperator(-, [x,y])
 end
 
-
-
-@wrapped function *(x::AbstractOperator, y::AbstractOperator)
+function *(x::SymbolicOperator, y::SymbolicOperator)
     if x  isa Number && y isa Number
         x*y
     else
         if x isa Number
-            similarterm(y, *, [x,y],)
+            SymbolicOperator(*, [x,y])
         else 
-            similarterm(x, *, [x,y],)
+            SymbolicOperator(*, [x,y])
         end
     end
 end
-@wrapped function *(x::Number, y::AbstractOperator)
-   similarterm(y, *, [x,y],)
+ function *(x::Number, y::SymbolicOperator)
+   SymbolicOperator(*, [x,y])
 end
-@wrapped function *(x::AbstractOperator, y::Number)
-   similarterm(x, *, [x,y],)
-end
-
-@wrapped function inv(G::AbstractOperator)
-    similarterm(G, inv, [G])
-end
-@wrapped function adjoint(G::AbstractOperator)
-    similarterm(G, adjoint, [G])
-end
-@wrapped function log(G::AbstractOperator)
-    similarterm(G, log, [G])
-end
-@wrapped function tr(G::AbstractOperator)
-    similarterm(G, tr, [G])
-end
-@wrapped function (/)(left::AbstractOperator, right::AbstractOperator)
-    return   similarterm(left, /, [left, right], )
+ function *(x::SymbolicOperator, y::Number)
+   SymbolicOperator(*, [x,y])
 end
 
-SymbolicUtils.simplify(n::SymbolicOperator; kw...) = wrap(SymbolicUtils.simplify(unwrap(n); kw...))
-SymbolicUtils.simplify_fractions(n::SymbolicOperator; kw...) = wrap(SymbolicUtils.simplify_fractions(unwrap(n); kw...))
-SymbolicUtils.expand(n::SymbolicOperator) = wrap(SymbolicUtils.expand(unwrap(n)))
-substitute(expr::SymbolicOperator, s::Pair; kw...) = wrap(substituter(s)(unwrap(expr); kw...)) # backward compat
-substitute(expr::SymbolicOperator, s::Vector; kw...) = wrap(substituter(s)(unwrap(expr); kw...))
-substitute(expr::SymbolicOperator, s::Dict; kw...) = wrap(substituter(s)(unwrap(expr); kw...))
-SymbolicUtils.Code.toexpr(x::SymbolicOperator) = SymbolicUtils.Code.toexpr(unwrap(x))
-SymbolicUtils.Code.toexpr(x::SymbolicOperator,st) = SymbolicUtils.Code.toexpr(unwrap(x),st)
-SymbolicUtils.setmetadata(x::SymbolicOperator, t, v) = wrap(SymbolicUtils.setmetadata(unwrap(x), t, v))
-SymbolicUtils.getmetadata(x::SymbolicOperator, t) = SymbolicUtils.getmetadata(unwrap(x), t)
-SymbolicUtils.hasmetadata(x::SymbolicOperator, t) = SymbolicUtils.hasmetadata(unwrap(x), t)
+ function inv(G::SymbolicOperator)
+    SymbolicOperator(inv, [G])
+end
+ function adjoint(G::SymbolicOperator)
+    SymbolicOperator(adjoint, [G])
+end
+ function log(G::SymbolicOperator)
+    SymbolicOperator(log, [G])
+end
+ function tr(G::SymbolicOperator)
+    SymbolicOperator(tr, [G])
+end
+ function (/)(left::SymbolicOperator, right::SymbolicOperator)
+    return SymbolicOperator(/, [left, right])
+end
 
 Broadcast.broadcastable(x::SymbolicOperator) = x
 
-Base.zero(::AbstractOperator) = zero(typeof(AbstractOperator))
-function Base.zero(::typeof(AbstractOperator))
+Base.zero(::SymbolicOperator) = zero(typeof(SymbolicOperator))
+function Base.zero(::typeof(SymbolicOperator))
     return 0
 end
 
-simplify_kernel(expr) = expr |> unwrap |> _simplify_kernel |> wrap
+simplify_kernel(expr) = _simplify_kernel(expr)
 function _simplify_kernel(expr)
     is_number(x) = x isa Number
-    is_operator(::BasicSymbolic{K}) where K <: AbstractOperator = true  
-    is_operator(::K) where K <: AbstractOperator = true  
+    is_operator(::K) where K <: SymbolicOperator = true  
     is_operator(x) = false  
     rules = [
         @acrule 0 * ~x => 0
@@ -153,31 +258,22 @@ function _simplify_kernel(expr)
     simplify(expr, rewriter = rules)
 end
 
-Base.isequal(a::SymbolicOperator,b::SymbolicOperator) = isequal(unwrap(a), unwrap(b))
-convert(::Type{SymbolicOperator}, x::Number) = SymbolicOperator(x)
+convert(::Type{SymbolicOperator}, x::Number) = make_leaf(x)
 function Base.promote_rule(::Type{SymbolicOperator}, ::Type{K}) where K<:Number 
      return SymbolicOperator
 end
-#Dirty ?
-function *(A::Matrix{SymbolicOperator},B::Matrix{SymbolicOperator})
-    return wrap.(unwrap.(A)*unwrap.(B))
-end
-function *(A::Matrix,B::Matrix{SymbolicOperator})
-    return wrap.(A*unwrap.(B))
-end
-function *(A::Matrix{SymbolicOperator},B::Matrix)
-    return wrap.(unwrap.(A)*B)
-end
 
+Base.display(A::SymbolicOperator) = error("Display not implemented yet for SymbolicOperator")
 
-Base.display(A::SymbolicOperator) = display(unwrap(A))
-
-@testitem "Wrapper and promotion rule" begin
+@testitem "Basic matching" begin
     using Symbolics
     using LinearAlgebra
-    @variables G::Kernel x
-    @test 1 * G |> simplify_kernel == G
-    @test 0 * G |> simplify_kernel == 0
+    @variables x
+    G = make_leaf(:G)
+    expr = 1 * G 
+    @test expr |> simplify_kernel == G
+    expr = 0 * G 
+    @test expr |> simplify_kernel == 0
     @test isequal(0 - G |> simplify_kernel, -G)
     @test zero(G) == 0
     A = [G G; G G]
@@ -190,7 +286,8 @@ end
     #TODO: add detection of singular kernel ?
     using Symbolics
     using LinearAlgebra
-    @variables G::Kernel Σ::Kernel
+    G = make_leaf(:G)
+    Σ = make_leaf(:Σ)
     @test isequal(inv(inv(G)) |> simplify_kernel, G)
     @test isequal(simplify_kernel( G - G), 0)
     @test isequal(simplify_kernel( G + G + G - G),2G)
@@ -199,8 +296,10 @@ end
 @testitem "Construction of the current observable" begin
     using Symbolics
     using LinearAlgebra
-    @variables G_R::Kernel G_K::Kernel
-    @variables Σ_R::Kernel Σ_K::Kernel
+    G_R = make_leaf(:G_R)
+    G_K = make_leaf(:G_K)
+    Σ_R = make_leaf(:Σ_R)
+    Σ_K = make_leaf(:Σ_K)
     τz = [1 2; 2 3] // 2
     G = [0 G_R'; G_R G_K]
     Σl = [Σ_K Σ_R; Σ_R' 0]
