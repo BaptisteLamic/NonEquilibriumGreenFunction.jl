83bb51f30a83e5083210dca3585bd642b438f538
Baptiste Lamic <baptiste.lamic@gmail.com>
2026-02-25 10:01:21 +0100

    Refactor KernelFunction and KernelDomain structures for improved clarity and functionality

--- src/kernelFunctions.jl
+++ src/kernelFunctions.jl
@@ -1,33 +1,53 @@
 export KernelFunction
+export KernelDomain
 
 using ACAFact
 
-struct KernelFunction{F,A,T}
+struct KernelDomain{T}
+    x_min::T
+    x_max::T
+    y_min::T
+    y_max::T
+    n_steps::Int
+end
+
+function KernelDomain(x_lims, y_lims=x_lims; n_steps::Int)
+    return KernelDomain(first(x_lims), last(x_lims), first(y_lims), last(y_lims), n_steps)
+end
+
+function Base.size(domain::KernelDomain,i)
+    if i in (1,2)
+        return domain.n_steps
+    end
+    return 1
+end
+function Base.size(domain::KernelDomain)
+    return (size(domain,i) for i in 1:2)
+end
+
+struct KernelFunction{F,D,T}
     f::F
-    domain::Tuple{A,A}
+    domain::KernelDomain{D}
     eltype::Type{T}
     blocksize::Int
 end
-function KernelFunction(f, dom1, dom2)
-    f0 = f(dom1[1], dom2[1])
+function KernelFunction(f, domain::KernelDomain)
+    f0 = f(domain.x_min, domain.y_min)
     @assert size(f0, 1) == size(f0, 2) "Kernel function must return square matrices."
     blocksize = size(f0, 1)
     T = eltype(f0)
-    return KernelFunction(f, (dom1, dom2), T, blocksize)
-end
-"""
-Instantiate a KernelFunction from a function `f(x,y)` taking two arguments, and a domain tuple `(a,b)`.
-"""
-function KernelFunction(f, kernelDomain)
-    return KernelFunction(f, kernelDomain, kernelDomain)
+    return KernelFunction(f, domain, T, blocksize)
 end
 
 function Base.size(kf::KernelFunction, dim)
-    n = length(kf.domain[dim]) * kf.blocksize
-    return n
+    if dim in (1,2)
+        n = size(domain, dim) * kf.blocksize
+        return n
+    end
+    return 1
 end
 function Base.size(kf::KernelFunction)
-    return (size(kf, 1), size(kf, 2))
+    return (size(kf, i) for i in 1:2)
 end
 function Base.eltype(kf::KernelFunction)
     return kf.eltype
@@ -57,10 +77,10 @@ function ACAFact.row!(buf, kf::KernelFunction, j)
 end
 
 @testitem "Test symetric KernelFunction" begin
-    domain = (0.0:0.1:1.0)
-    modulation_11(x,y) = exp(-abs(x))*cos(y)
-    modulation_22(x,y) = (atan(x) * sin(y) + 1)
-    kf = KernelFunction((x, y) -> [modulation_11(x,y) 1.0; 0.0 modulation_22(x, y)], domain)
+    domain = KernelDomain((0.0, 1.0), n_steps=512)
+    modulation_11(x, y) = exp(-abs(x)) * cos(y)
+    modulation_22(x, y) = (atan(x) * sin(y) + 1)
+    kf = KernelFunction((x, y) -> [modulation_11(x, y) 1.0; 0.0 modulation_22(x, y)], domain)
     @test kf.blocksize == 2
     @test kf.eltype == Float64
     @test size(kf, 1) == length(domain) * kf.blocksize
@@ -86,17 +106,17 @@ function restrict_domain(kf, dom1, dom2)
 end
 
 @testitem "Test domain reduction" begin
-    domain = (0.0:0.1:1.0)
-    modulation_11(x,y) = exp(-abs(x))*cos(y)
-    modulation_22(x,y) = (atan(x) * sin(y) + 1)
-    kf = KernelFunction((x, y) -> [modulation_11(x,y) 0.0; 0.0 modulation_22(x, y)], domain)
+    domain = KernelDomain((0.0, 1.0))
+    modulation_11(x, y) = exp(-abs(x)) * cos(y)
+    modulation_22(x, y) = (atan(x) * sin(y) + 1)
+    kf = KernelFunction((x, y) -> [modulation_11(x, y) 0.0; 0.0 modulation_22(x, y)], domain)
     buf_full_kernel = zeros(kf.eltype, size(kf, 2))
     reduced_domain = 0.0:0.1:0.5
     reduced_kf = NonEquilibriumGreenFunction.restrict_domain(kf, domain, reduced_domain)
     buf_reduced_kernel = zeros(kf.eltype, size(reduced_kf, 2))
     NonEquilibriumGreenFunction.ACAFact.row!(buf_full_kernel, kf, 1)
     NonEquilibriumGreenFunction.ACAFact.row!(buf_reduced_kernel, reduced_kf, 1)
-    @test length(buf_reduced_kernel) == length(reduced_domain)*kf.blocksize
+    @test length(buf_reduced_kernel) == length(reduced_domain) * kf.blocksize
     @test buf_reduced_kernel[1:reduced_kf.blocksize:end] == modulation_11.(reduced_domain[1], reduced_domain)
     @test norm(modulation_11.(domain[1], domain)) > 1
     @test size(kf, 2) > size(reduced_kf, 2)
@@ -106,7 +126,7 @@ function fill_with_kernel!(array, kf)
     bs = kf.blocksize
     for j in 1:length(kf.domain[2])
         for i in 1:length(kf.domain[1])
-            array[(i-1)*bs+1:i*bs,(j-1)*bs+1:j*bs] .= kf.f(kf.domain[1][i], kf.domain[2][j])
+            array[(i-1)*bs+1:i*bs, (j-1)*bs+1:j*bs] .= kf.f(kf.domain[1][i], kf.domain[2][j])
         end
     end
     return array
@@ -115,23 +135,23 @@ end
 @testitem "fill_with_kernel!" begin
     modulation(x) = sin(x)
     kf = KernelFunction(
-        (x, y) -> [modulation(x - pi*y) 2.0; -1.0 modulation(2x - y)],
+        (x, y) -> [modulation(x - pi * y) 2.0; -1.0 modulation(2x - y)],
         (0.0:0.1:1.0),
         (0.0:0.2:10.0)
     )
     buf_row = zeros(kf.eltype, size(kf, 2))
     matrix = zeros(kf.eltype, size(kf))
     NonEquilibriumGreenFunction.fill_with_kernel!(matrix, kf)
-    for i in 1:size(matrix,1)
+    for i in 1:size(matrix, 1)
         NonEquilibriumGreenFunction.ACAFact.row!(buf_row, kf, i)
-        @test matrix[i,:] == buf_row
+        @test matrix[i, :] == buf_row
     end
 
     buf_col = zeros(kf.eltype, size(kf, 1))
     matrix = zeros(kf.eltype, size(kf))
     NonEquilibriumGreenFunction.fill_with_kernel!(matrix, kf)
-    for i in 1:size(matrix,1)
+    for i in 1:size(matrix, 1)
         NonEquilibriumGreenFunction.ACAFact.col!(buf_col, kf, i)
-        @test matrix[:,i] == buf_col
+        @test matrix[:, i] == buf_col
     end
 end
